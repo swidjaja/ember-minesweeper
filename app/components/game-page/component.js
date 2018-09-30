ecf3b5ec304116d5e2991459a070036db02ff6d6
sugi <widjaja.s@gmail.com>
2018-09-30 02:42:39 -0700

    Add button icon for each game status

--- app/components/game-page/component.js
+++ app/components/game-page/component.js
@@ -1,10 +1,28 @@
 import Ember from 'ember';
+import { GAME_STATUS } from 'minesweeper-game/lib/constants';
 
 export default Ember.Component.extend({
   classNames: ['game-page'],
   gameService: Ember.inject.service(),
   gameStatus: Ember.computed.oneWay('gameService.gameStatus'),
   gridCells: Ember.computed.oneWay('gameService.gridCells'),
+
+  restartButtonClass: Ember.computed('gameService.gameStatus', function () {
+    const gameStatus = this.get('gameStatus');
+
+    switch (gameStatus) {
+      case GAME_STATUS.NOT_STARTED:
+      case GAME_STATUS.IN_PROGRESS:
+        return 'bg-happy_face';
+      case GAME_STATUS.LOST:
+      case GAME_STATUS.OUT_OF_TIME:
+        return 'bg-sad_face';
+      case GAME_STATUS.WIN:
+        return 'bg-cool_face';
+      default: 
+        return '';
+    }
+  }),
 
   elapsedTime: Ember.computed('gameService.elapsedTime', function () {
     const elapsedTime = '' + this.get('gameService.elapsedTime');
