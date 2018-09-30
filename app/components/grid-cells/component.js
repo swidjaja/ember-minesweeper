ddb51a38ef5eb7c483dd4c5a7754bbcee5101ac3
sugi <widjaja.s@gmail.com>
2018-09-30 12:37:53 -0700

    Fix color contrast on cells

--- app/components/grid-cells/component.js
+++ app/components/grid-cells/component.js
@@ -4,22 +4,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['grid-cells'],
 
-  // updateNotification(actionType) {
-  //   const notificationPanel = this.$('.notification-panel');
-
-  //   notificationPanel.text('');
-  //   notificationPanel.css('display', 'block');
-  //   if (actionType === CELL_ACTION_TYPES.REVEAL) {
-  //     notificationPanel.text('Cell Revealed');
-  //   } else if (actionType === CELL_ACTION_TYPES.FLAGGED) {
-  //     notificationPanel.text('Cell Flagged');
-  //   }
-  // },
-
   actions: {
     gridCellClicked(payload) {
       this.sendAction('gridCellClicked', payload);
-      // this.updateNotification(CELL_ACTION_TYPES.REVEAL);
     }
   }
 });
