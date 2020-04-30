d8a2dd9ecea5b444223f8063802d99b8ab03f9e0
sugi <widjaja.s@gmail.com>
2020-04-29 23:20:11 -0700

    test: test

--- app/services/game-service.js
+++ app/services/game-service.js
@@ -7,6 +7,7 @@ import {
   GAME_DIFFICULTY_CONFIGS
 } from 'minesweeper-game/lib/constants';
 
+// Game service
 export default Ember.Service.extend({
   timerService: Ember.inject.service(),
   elapsedTime: Ember.computed.oneWay('timerService.elapsedTime'),
